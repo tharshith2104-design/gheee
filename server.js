1890104d37af7096921647a3a1218f0d3fb5cb67
THARSHITH <tharshith2104@gmail.com>
2026-09-08 14:08:00 +0530

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const MIME_TYPES = {
     '.html': 'text/html',
